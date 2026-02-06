c5ff388dab01397da9c45d024c39997135f6fa00
Arkady Sokolenko <github@diskills-developer.org>
2026-02-06 13:43:30 +0300

    lab1: added train

--- lab1/wta.py
+++ lab1/wta.py
@@ -17,6 +17,9 @@ class Neuron:
     def __call__(self, input: list[float]) -> float:
         return sum(self.weights[i] * input[i] for i in range(len(input)))
 
+    def train(self, input: list[float], eta: float):
+        self.weights = [self.weights[i] + eta * (input[i] - self.weights[i]) for i in range(len(input))]
+
 
 class Network:
     def __init__(self, input_size: int, number_of_neurons: int):
@@ -27,3 +30,24 @@ class Network:
         y = [0] * len(u)
         y[u.index(max(u))] = 1
         return y
+
+    def train(self, inputs: list[list[float]], eta: float):
+        for input in inputs:
+            u = [neuron(input) for neuron in self.neurons]
+            winner = self.neurons[u.index(max(u))]
+            winner.train(input, eta)
+
+
+def main():
+    network = Network(2, 4)
+    training_set = [
+        [0.97, 0.2], [1.0, 0.0], [-0.72, 0.7], [-0.67, 0.74],
+        [-0.8, 0.6], [0.0, -1.0], [0.2, -0.97], [-0.3, -0.95],
+    ]
+    network.train(training_set, 0.5)
+    for i, neuron in enumerate(network.neurons):
+        print(i, neuron.weights)
+
+
+if __name__ == "__main__":
+    main()
